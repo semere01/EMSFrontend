cde0f14ed988f9d6fdea053e92a673cc853bb3fc
aben-bel <abenezer.sbelay@gmail.com>
2021-06-15 17:53:11 +0300

    add department

---
+++ employee_management_system/src/_pages/department/Department.js
@@ -0,0 +1,279 @@
+import React, { useCallback, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import { Button, Modal } from "@material-ui/core";
+import { useHistory, useLocation } from "react-router-dom";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import ButtonWrapper from "../../_components/FormsUI/ButtonWrapper";
+import TextFieldWrapper from "../../_components/FormsUI/TextFieldWrapper";
+import { dataService } from "../../_services/data.service";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: "1.5rem",
+    marginBottom: "1.5rem",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  link: {
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+  },
+  formControl: {
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(3),
+  },
+  topBottomMargin: {
+    marginTop: "1.5rem",
+    marginBottom: "1.5rem",
+    backgroundColor: "red",
+  },
+}));
+
+const FORM_VALIDATION = Yup.object().shape({
+  departmentTitle: Yup.string()
+    .required("Required")
+    .min(3, "department title should at least be greater than 3 characters"),
+  numEmployees: Yup.number()
+    .typeError("Must be a positive number")
+    .required("Required")
+    .positive("Number of employees must be positive"),
+});
+export default function Department(props) {
+  const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const [open, setOpen] = useState(false);
+
+  console.log(location.state.key);
+  const department = location.state.key;
+
+  const INITIAL_FROM_STATE = {
+    departmentTitle: department.department_title,
+    numEmployees: department.no_of_employees,
+  };
+  const handleDelete = () => {
+    if (department.action === "Save") {
+      dataService
+        .deleteDepartment(department.id)
+        .then((res) => {
+          console.log("Deleted: ", res);
+          history.push("/departments");
+        })
+        .catch((error) => {});
+    }
+  };
+  return (
+    <React.Fragment>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+              Department Detail
+            </Typography>
+            <Formik
+              initialValues={{ ...INITIAL_FROM_STATE }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values, { setStatus }) => {
+                // console.log("values employee: ", values);
+                if (department.action === "Create") {
+                  dataService
+                    .addDepartment(values)
+                    .then((res) => {
+                      console.log("response status: ", res);
+                      if (res.status === 201) {
+                        setStatus({
+                          sent: true,
+                          msg: "Sucessfully Added Employee",
+                        });
+                      }
+                    })
+                    .catch((err) => {
+                      console.log("was here to report an error employee");
+                      setStatus({
+                        sent: false,
+                        msg: `${err}. Please try again later.`,
+                      });
+                    });
+                } else if (department.action === "Save") {
+                  values.id = department.id;
+                  dataService
+                    .editDepartment(values)
+                    .then((res) => {
+                      console.log("response status: ", res);
+                      if (res.status === 201) {
+                        setStatus({
+                          sent: true,
+                          msg: "Sucessfully Added Employee",
+                        });
+                      }
+                    })
+                    .catch((err) => {
+                      console.log("was here to report an error employee");
+                      setStatus({
+                        sent: false,
+                        msg: `${err}. Please try again later.`,
+                      });
+                    });
+                }
+              }}
+            >
+              {({ status }) => (
+                <Form>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                      <TextFieldWrapper
+                        required
+                        id="departmentTitle"
+                        name="departmentTitle"
+                        label="Department Title"
+                        fullWidth
+                        autoComplete="department-title"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextFieldWrapper
+                        required
+                        id="numEmployees"
+                        name="numEmployees"
+                        label="Number of Employees"
+                        fullWidth
+                        autoComplete="num-employees"
+                      />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <React.Fragment>
+                        {department.action === "Save" ? (
+                          <Button
+                            color="secondary"
+                            onClick={() => {
+                              setOpen(true);
+                            }}
+                            className={classes.button}
+                          >
+                            Delete
+                          </Button>
+                        ) : (
+                          <span></span>
+                        )}
+                        <div className={classes.buttons}>
+                          <Button
+                            color="secondary"
+                            onClick={() => {
+                              history.push("/departments");
+                            }}
+                            className={classes.button}
+                          >
+                            <span>Cancel</span>
+                          </Button>
+                          <ButtonWrapper
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            // onClick={console.log("was clicked employee")}
+                            className={classes.button}
+                          >
+                            {department.action}
+                          </ButtonWrapper>
+                        </div>
+                      </React.Fragment>
+                    </Grid>
+                  </Grid>
+                  {status && status.msg && (
+                    <p
+                      className={`alert ${
+                        status.sent ? "alert-success" : "alert-error"
+                      }`}
+                    >
+                      {status.msg}
+                    </p>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          </React.Fragment>
+        </Paper>
+        <Modal
+          open={open}
+          onClose={() => {
+            setOpen(false);
+          }}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: `40%`,
+              left: `40%`,
+              background: "#c1d3f0",
+              width: "400",
+              border: "1px solid #000",
+              boxShadow: "3",
+              padding: "2, 4, 3",
+            }}
+            className={classes.paper}
+          >
+            <h2 id="simple-modal-title">Warning!</h2>
+            <p id="simple-modal-description">
+              Are you sure? You are about to remove a department.
+            </p>
+            <Button
+              color="secondary"
+              onClick={() => {
+                setOpen(false);
+                history.push("/departments");
+                handleDelete();
+              }}
+              className={classes.button}
+            >
+              Delete
+            </Button>
+            <Button
+              color="secondary"
+              onClick={() => {
+                setOpen(false);
+              }}
+              className={classes.button}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Modal>
+      </main>
+    </React.Fragment>
+  );
+}
---
+++ employee_management_system/src/_pages/department/Departments.js
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import {
+  Avatar,
+  Box,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Fab,
+  makeStyles,
+  TableHead,
+  Toolbar,
+  TablePagination,
+} from "@material-ui/core";
+import { Search as SearchIcon } from "react-feather";
+
+import AddIcon from "@material-ui/icons/Add";
+import { useHistory } from "react-router";
+import { grey } from "@material-ui/core/colors";
+import { dataService } from "../../_services/data.service";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  tableHead: {
+    backgroundColor: "#343a40",
+  },
+  wholeTable: {
+    marginTop: "1.5rem",
+  },
+  white: {
+    color: "white !important",
+  },
+}));
+
+export default function Departments() {
+  const classes = useStyles();
+
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState(rows);
+  const history = useHistory();
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    dataService.getDepartments().then((data) => {
+      setRows(data);
+      setFilteredRows(data);
+      console.log("Departments: ", data);
+    });
+  }, []);
+
+  function filter(e) {
+    e.preventDefault();
+    console.log("query: ", e.target.value);
+    const val = e.target.value.toLowerCase();
+    setFilteredRows(
+      rows.filter((row) => row.department_title?.toLowerCase().includes(val))
+    );
+  }
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  function openDetail(data, action) {
+    data.action = action;
+    history.push({
+      pathname: "/Department",
+      state: {
+        key: data,
+      },
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <Toolbar>
+        <h1>Department</h1>
+        <div className={classes.grow} />
+
+        <Fab
+          onClick={() => openDetail({}, "Create")}
+          color="secondary"
+          aria-label="add user"
+          variant="extended"
+        >
+          <AddIcon className={classes.extendedIcon} />
+          Add new Department
+        </Fab>
+      </Toolbar>
+
+      <Box sx={{ maxWidth: 500 }}>
+        <TextField
+          fullWidth
+          onChange={filter}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SvgIcon fontSize="small" color="action">
+                  <SearchIcon />
+                </SvgIcon>
+              </InputAdornment>
+            ),
+          }}
+          placeholder="Search department"
+          variant="outlined"
+        />
+      </Box>
+
+      <Table className={classes.wholeTable} size="small">
+        <TableHead className={classes.tableHead}>
+          <TableRow>
+            <TableCell className={classes.white}>Department Title</TableCell>
+            <TableCell className={classes.white}>Number of Employees</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredRows.slice(page * limit, page * limit + limit).map((row) => (
+            <TableRow key={row.id} onClick={() => openDetail(row, "Save")}>
+              <TableCell>{row.department_title}</TableCell>
+              <TableCell>{row.no_of_employees}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        component="div"
+        count={filteredRows.length}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </React.Fragment>
+  );
+}
